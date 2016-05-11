c2a38544de37a33d00d2902ad565e7ac1b0947c2
Giulio Vian <giuliovdev@gmail.com>
2016-05-11 17:31:27 +0200

    Initial version from original post
    http://blog.casavian.eu/2014/04/28/increment-version-for-changed-assemblies-only-third-part/

---
+++ ConsoleApplication1/test3.bat
@@ -0,0 +1,4 @@
+set TF_BUILD_BUILDNUMBER=CIBuild_20130613.6
+
+MSBuild /property:CustomAfterMicrosoftCommonTargets=C:\Users\gvian\Source\NoVC\BuildSamples\ConsoleApplication1\autoIncrTfs2013.targets C:\Users\gvian\Source\NoVC\BuildSamples\ConsoleApplication1\ConsoleApplication1.csproj
+
