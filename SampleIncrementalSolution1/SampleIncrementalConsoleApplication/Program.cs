6c08cd0785fb5ee801dab095c6b64c3254277c04
Giulio Vian <giuliovdev@gmail.com>
2016-05-13 12:27:53 +0200

    Basic intertwined Solutions, no autoincr

---
+++ SampleIncrementalSolution1/SampleIncrementalConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleIncrementalConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cc = new SampleIncrementalCommonLibrary.Class1();
+            Console.WriteLine(cc.ToString());
+            var c1 = new SampleIncrementalConsoleClassLibrary.Class1();
+            Console.WriteLine(c1.ToString());
+        }
+    }
+}
