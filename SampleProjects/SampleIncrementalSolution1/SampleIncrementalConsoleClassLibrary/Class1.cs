2fc81757797fb125ff2f7703c567c56a0adc1794
Administrator <>
2016-05-17 15:23:46 +0200

    Updated Class1.cs

--- SampleProjects/SampleIncrementalSolution1/SampleIncrementalConsoleClassLibrary/Class1.cs
+++ SampleProjects/SampleIncrementalSolution1/SampleIncrementalConsoleClassLibrary/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,6 @@ namespace SampleIncrementalConsoleClassLibrary
 {
     public class Class1
     {
+        // change #001
     }
 }
